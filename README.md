2b5b3d57d9093aa52c9ae74875ac9cfa86384312
Karl Fischer <fishi0x01@gmail.com>
2020-02-17 18:55:34 +0100

    More tests

--- README.md
+++ README.md
@@ -4,3 +4,16 @@
 
 # docker-extract
 Work in progress ..
+
+Example extract `alpine:latest` to dir `./docker-fs`:
+```rust
+use docker_extract;
+use std::path::Path;
+
+fn main() {
+    let image = "alpine";
+    let tag = "latest";
+    let to_dir = Path::new("./docker-fs");
+    docker_extract::extract_image(image, tag, &to_dir);
+}
+```
--- tests/extract_image.rs
+++ tests/extract_image.rs
@@ -2,6 +2,7 @@
 use docker_extract::extract_image;
 use std::path::Path;
 use tempdir::TempDir;
+use std::io;
 
 #[test]
 fn test_extract_ubuntu() {
@@ -21,6 +22,17 @@ fn test_extract_alpine() {
     extract_image("alpine", "3.11.3", Path::new(tmp_dir_str.as_str())).unwrap();
     assert_eq!(
         true,
-        Path::new(format!("{}/etc/apk/world", tmp_dir_str.as_str()).as_str()).exists()
+        Path::new(format!("{}/lib/apk/db/installed", tmp_dir_str.as_str()).as_str()).exists()
+    );
+}
+
+#[test]
+fn test_extract_non_existing() {
+    let tmp_dir = TempDir::new("docker-extract-test").unwrap();
+    let tmp_dir_str = String::from(tmp_dir.path().to_str().unwrap());
+    let result = extract_image("does-not", "exist", Path::new(tmp_dir_str.as_str()));
+    assert_eq!(
+        result.err().unwrap().to_string(),
+        io::Error::new(io::ErrorKind::Other, "Error running 'docker save does-not:exist'").to_string()
     );
 }
